aca7f66c064fb4cab031e8099cdc3927d8a118cd
Yenner Hernández <yenner.hernandez@hotmail.com>
2021-07-22 22:28:37 -0600

    previo a implementación de NavigationDrawer

--- app/src/main/java/com/yhx/cinetva/MainActivity.kt
+++ app/src/main/java/com/yhx/cinetva/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //title = "Cartelera para hoy"
+        title = "Menú"
         btIraCartelera.setOnClickListener{
             startActivity(Intent(this, CarteleraActivity::class.java))
         }
