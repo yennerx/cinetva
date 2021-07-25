a9700cf03a3be422788fceddfd6a98638a7b3564
Yenner Hernández <yenner.hernandez@hotmail.com>
2021-07-24 18:50:17 -0600

    Se agregan campos para rellenar en la pantalla de Agregar Película, pendiente establecer forma de guardar los datos

--- app/src/main/java/com/yhx/cinetva/activities/CarteleraActivity.kt
+++ app/src/main/java/com/yhx/cinetva/activities/CarteleraActivity.kt
@@ -14,7 +14,7 @@ class CarteleraActivity : AppCompatActivity() {
     private fun peliculasPrueba():ArrayList<DataPelicula>{
         val lista = ArrayList<DataPelicula>()
         lista.add(DataPelicula(1,"Shrek","Un ogro se dispone a salvar a una princesa a cambio...","A","https://cdn.pixabay.com/photo/2015/04/14/17/08/alien-722415_960_720.jpg"))
-        lista.add(DataPelicula(2,"Duro de Matar 6", "veremos cómo un joven policía llamado John ...", "C", "https://cdn.pixabay.com/photo/2015/11/18/15/02/actor-1049257_960_720.jpg"),)
+        lista.add(DataPelicula(2,"Duro de Matar 6", "veremos cómo un joven policía llamado John ...", "C", "https://cdn.pixabay.com/photo/2015/11/18/15/02/actor-1049257_960_720.jpg"))
         lista.add(DataPelicula(3,"Avengers Infinity War","Los superhéroes se alían para vencer al poderoso Thanos...", "B", "https://cdn.pixabay.com/photo/2019/05/31/02/08/iron-man-4241268_960_720.jpg"))
         lista.add(DataPelicula(4,"Mortal Kombat","Nueva adaptación del popular videojuego con el aval de la presencia como productor de James Wan.", "B", "https://i.blogs.es/f9d649/mortal-kombay/1366_2000.jpeg"))
         lista.add(DataPelicula(5,"Chaos Walking","Una película a la que no dejan de acompañar los problemas. Se terminó de rodar en noviembre de 2017 y no se estrena hasta ahora", "C", "https://i.blogs.es/75cbdd/chaos-walking/1366_2000.jpeg"))
